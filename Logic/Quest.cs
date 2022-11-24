41d603e169cf9919aa445505402e6a2e9b37b143
ast <atrusov@gmail.com>
2022-11-24 08:50:07 +0300

    заготовка для Quest

--- Logic/Quest.cs
+++ Logic/Quest.cs
@@ -1,6 +1,18 @@
+using NLua;
+
 namespace QE.Logic;
 
 public class Quest
 {
-    //...
+    Lua lua;
+    public string CurrentLocationId { get; set; }
+    public Dictionary<string, Location> Locations;
+    public Dictionary<string, Way> Ways;
+    public Dictionary<string, Item> Items;
+    public Dictionary<string, State> States;
+
+    public Quest()
+    {
+        //...
+    }
 }
