6063bbf1685391aed3ea8cfae34b22fc48d604f9
ast <atrusov@gmail.com>
2022-11-24 08:17:46 +0300

    реализовали Item

--- Logic/Item.cs
+++ Logic/Item.cs
@@ -1,6 +1,11 @@
+using QE.Logic.Tool;
+
 namespace QE.Logic;
 
 public class Item
 {
-    //...
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public IValue<string> Description { get; set; }
+    public string LocationId { get; set; } 
 }
---
+++ Logic/Tool/Consts.cs
@@ -0,0 +1,7 @@
+namespace QE.Logic.Tool;
+
+public abstract class Consts
+{
+    public const string LOCATION_INVENTORY = "INVENTORY";
+    public const string LOCATION_VOID = "VOID";
+} 
